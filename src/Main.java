55f74b491ffaf1be0a52d0e3fbb7d1b011f6e2c0
mayar hussein <mayar_memoo@ymail.com>
2019-05-09 12:58:06 +0200

    updated  main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello beoble!!");
         System.out.println("hano banoo");
+        System.out.println("blo2 blo2 mayoura aleelet el adab w el joe 7eywan");
     }
 }
